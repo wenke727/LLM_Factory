45a9ed7c85995f384a76da33aafed90549d5f73e
wenke727 <btjs727@hotmail.com>
2024-05-21 13:15:19 +0800

    2024-05-20

--- src/qwen_llm.py
+++ src/qwen_llm.py
@@ -3,29 +3,25 @@ from loguru import logger
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-class ChatModel(AutoModelForCausalLM):
+class QwenModelWarpper:
     def __init__(self, model_name, device="cuda"):
-        super().__init__()
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
             torch_dtype=torch.bfloat16,
             device_map="auto"
-        ).to(device)
-        self.device = device
+        )
+        self.model.eval()
 
     def chat(self, query=None, history=[], verbose=False):
         """启动多轮对话，可指定初始提示和历史对话"""
         if len(history) == 0:
-            self.history = [{"role": "system", "content": "You are a helpful assistant."}]
-        else:
-            self.history = history
-
-        self.history.append({"role": "user", "content": query})
+            history = [{"role": "system", "content": "You are a helpful assistant."}]
+        history.append({"role": "user", "content": query})
 
         # 应用对话模板
         text = self.tokenizer.apply_chat_template(
-            self.history,
+            history,
             tokenize=False,
             add_generation_prompt=True
         )
@@ -47,16 +43,16 @@ class ChatModel(AutoModelForCausalLM):
 
         # 打印模型回答并更新对话历史
         if verbose:
-            logger.debug("AI: ", response)
+            logger.debug(f"AI: {response}")
 
-        self.history.append({"role": "system", "content": response})
+        history.append({"role": "system", "content": response})
 
-        return self.history  # 返回更新后的对话历史，以便于可能的后续用途
+        return response, history
 
 if __name__ == "__main__":
     # 使用示例
     model_name = "Qwen/Qwen1.5-32B-Chat"
-    chat_model = ChatModel(model_name)
+    chat_model = QwenModelWarpper(model_name)
     history = [{"role": "system", "content": "You are a helpful assistant."}]
     prompt = "Tell me more about AI."
     updated_history = chat_model.chat(query=prompt, history=history)
