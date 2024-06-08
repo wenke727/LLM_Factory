e766eaabe8638bb16eacdbd32d531191f6f8d6ab
wenke727 <btjs727@hotmail.com>
2024-06-09 00:56:29 +0800

    2024-06-08

---
+++ tutorial/langchain/readme.md
@@ -0,0 +1,5 @@
+# langchain
+
+:rocket: [Tutorial](https://github.com/langchain-ai/langchain/blob/master/docs/docs/tutorials/agents.ipynb)
+
+
---
+++ tutorial/test.py
@@ -0,0 +1,10 @@
+#%%
+import tiktoken
+tiktoken.encoding_for_model("gpt-4")
+
+
+# %%
+from dotenv import load_dotenv
+load_dotenv('.env', verbose=True)
+
+# %%
